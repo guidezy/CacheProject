4e7aa3126f996a6569127e06d62ce108467bf7cf
guidezy <guidezyann@gmail.com>
2015-06-08 11:40:56 +0200

    prout

--- FIFO_strategy.c
+++ FIFO_strategy.c
@@ -25,7 +25,7 @@ void *Strategy_Create(struct Cache *pcache)
 
 void Strategy_Close(struct Cache *pcache)
 {
-	//Cache_List_Delete(&pcache->pstrategy);
+	Cache_List_Delete(&pcache->pstrategy);
 }
 
 
@@ -47,7 +47,6 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 }
 
 
-/
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
 }  
