bd59c1075809fae1252a4e3c17298b66985dc6b7
Gatien Chapon <chapon.gatien@etu.unice.fr>
2015-06-08 13:56:05 +0200

    LRU

--- LRU_strategy.c
+++ LRU_strategy.c
@@ -1,11 +1,7 @@
 /*!
- * \file FIFO.c
+ * \file LRU_strategy.c
  *
- * \brief  Stratégie de remplacement fifo..
- * 
- * \author Jean-Paul Rigault 
- *
- * $Id: FIFO_strategy.c,v 1.3 2008/03/04 16:52:49 jpr Exp $
+ * Implementation de la strategie LRU
  */
 
 #include <assert.h>
@@ -18,8 +14,8 @@
 
 void *Strategy_Create(struct Cache *pcache) 
 {
-    Cache_List *liste = Cache_List_Create();
-    return &liste;
+    struct Cache_List *liste = Cache_List_Create();
+    return liste;
 }
 
 
@@ -37,17 +33,10 @@ void Strategy_Invalidate(struct Cache *pcache)
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
-	int ib;
-    struct Cache_Block_Header *pbh;
-
-    /* On cherche d'abord un bloc invalide */
-    if ((pbh = Get_Free_Block(pcache)) != NULL) return pbh;
-
-    return &pcache->pstrategy;
+	
 }
 
 
-/
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
 }  
