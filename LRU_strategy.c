42517e91c38c413cbe30b2f5a4a71af4c3add01c
Gatien Chapon <chapon.gatien@etu.unice.fr>
2015-06-08 16:01:54 +0200

    Fin LRU

--- LRU_strategy.c
+++ LRU_strategy.c
@@ -15,41 +15,47 @@
 void *Strategy_Create(struct Cache *pcache) 
 {
     struct Cache_List *liste = Cache_List_Create();
+    pcache->pstrategy = liste; 
     return liste; 
 }
 
 
 void Strategy_Close(struct Cache *pcache)
 {
-	 Cache_List_Delete(pcache->pstrategy); 
+	  Cache_List_Delete(pcache->pstrategy); 
+	  
 }
 
 
 void Strategy_Invalidate(struct Cache *pcache)
 {
-    struct Cache_Block_Header *pbh;
-    pbh = Get_Free_Block(pcache);
-    Cache_List_Append(pcache->pstrategy, pbh); 
+    Cache_List_Clear(pcache->pstrategy); 
 
 }
 
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
-
-    return Cache_List_Remove_First(pcache->pstrategy); 
+	struct Cache_Block_Header * pbh; 
+	if ((pbh = Get_Free_Block(pcache)) != NULL){
+		Cache_List_Append(pcache->pstrategy, pbh); 
+		return pbh; 
+	}
+    pbh= Cache_List_Remove_First(pcache->pstrategy);
+    Cache_List_Append(pcache->pstrategy, pbh); 
+    return pbh; 
 }
 
 
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
-	Cache_List_Append(pcache->pstrategy, pbh); 
+	Cache_List_Move_To_End(pcache->pstrategy, pbh); 
 }  
 
  
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
-	Cache_List_Append(pcache->pstrategy, pbh); 
+	Cache_List_Move_To_End(pcache->pstrategy, pbh); 
 } 
 
 char *Strategy_Name()
