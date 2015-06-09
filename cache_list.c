66575c3bce9356e275b0a89f6cef91293ad4f0ce
guidezy <guidezyann@gmail.com>
2015-06-09 15:00:22 +0200

    quelques fonctions cache_list

---
+++ cache_list.c
@@ -0,0 +1,80 @@
+/*!
+ * \file 
+ * 
+ * \brief Une liste simplifiée des headers de bloc du cache
+ *
+ * \author Jean-Paul Rigault 
+ *
+ */
+
+#include <stdbool.h>
+
+/*! La cellule de liste doublement chainée */
+struct Cache_List
+{
+    struct Cache_Block_Header *pheader;	/* information */
+    struct Cache_List *next;		/* chainage avant */
+    struct Cache_List *prev;		/* chainage arrière */
+
+};
+/*! Création d'une liste de blocs */
+struct Cache_List *Cache_List_Create();
+/*! Destruction d'une liste de blocs */
+void Cache_List_Delete(struct Cache_List *list);
+
+/*! Insertion d'un élément à la fin */
+void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh);
+/*! Insertion d'un élément au début*/
+void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh);
+
+/*! Retrait du premier élément */
+struct Cache_Block_Header *Cache_List_Remove_First(struct Cache_List *list);
+/*! Retrait du dernier élément */
+struct Cache_Block_Header *Cache_List_Remove_Last(struct Cache_List *list);
+/*! Retrait d'un élément quelconque */
+struct Cache_Block_Header *Cache_List_Remove(struct Cache_List *list,
+                                             struct Cache_Block_Header *pbh);
+
+/*! Remise en l'état de liste vide */
+void Cache_List_Clear(struct Cache_List *list){
+	while(list)
+}
+
+/*! Test de liste vide */
+bool Cache_List_Is_Empty(struct Cache_List *list){
+	if(list->pheader == NULL) return true;
+
+	return false;
+}
+
+/*! Transférer un élément à la fin */
+void Cache_List_Move_To_End(struct Cache_List *list,
+                            struct Cache_Block_Header *pbh)
+{
+	Cache_List current = list;
+	while(current->next != NULL){
+		if(pbh == current->pheader){
+			Cache_List_Remove(list,pbh);
+		}
+		current = current->next;
+	}
+	if(current->pheader == pbh) return;
+	pbh->prev = current;
+	current->next = pbh;
+}
+/*! Transférer un élément  au début */
+void Cache_List_Move_To_Begin(struct Cache_List *list,
+                              struct Cache_Block_Header *pbh)
+{
+	if(list->pheader == pbh) return;
+	Cache_List current = list;
+	while(current->next != NULL){
+		if(pbh == current->pheader){
+			list->prev = Cache_List_Remove(current,pbh);
+			return;
+		}
+		current = current->next;
+	}
+	pbh->next = list;
+	list->prev = pbh;
+}
